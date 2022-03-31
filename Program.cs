a10afd1f8dd40b1f6fef4eea2705bd3dd54166e3
joshuayordana <joshuayordana8@gmail.com>
2022-03-31 14:34:00 +0700

    dari ko denzel

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var ourWindow = new NativeWindowSettings()
+			{
+				Size = new Vector2i(600, 600),
+				Title = "ConsoleApp1"
+			};
+
+			using (var window = new Window(GameWindowSettings.Default, ourWindow))
+			{
+				window.Run();
+			}
+		}
+	}
+}
